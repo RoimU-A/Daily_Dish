cee9f61bc0f95ab103fa7a349a6fbd63665261b9
RoimU-A <m.akihiro3349@gmail.com>
2025-07-13 16:54:18 +0900

    フロントエンドが読み取れないことに起因して画面に何も表示されない現象を改善5

--- daily_dish_project/settings.py
+++ daily_dish_project/settings.py
@@ -151,7 +151,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_collected')
 
 # React フロントエンド静的ファイル設定
 STATICFILES_DIRS = [
